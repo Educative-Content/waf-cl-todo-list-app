f226a77e1cee3abcf29daa65fd2a2facf287a191
Lord-Sekke <86236909+Lord-Sekke@users.noreply.github.com>
2023-08-10 14:34:08 +0500

    Update final.js

--- final.js
+++ final.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'abcd1234',
-  database: 'your_db_name'
+  database: 'my_db_name'
 });
 
 db.connect((err) => {
